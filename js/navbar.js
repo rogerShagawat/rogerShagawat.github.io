5c67197a2fb8d94d5c2f4aba9115519ac02a8f2a
rogerShagawat <rogershagawat@gmail.com>
2021-10-15 16:36:33 -0400

    pomes page addition

--- js/navbar.js
+++ js/navbar.js
@@ -1,35 +1,35 @@
-// document.write( '<nav id=\"navbar\">\n' );
-// document.write( '<a class=\"nav-elem\" href=\"navbar.html\">this is the nav bar</a> | <a class=\"nav-elem\" href=\"index.html\">home</a> |\n' );
-// document.write( '<!-- link to a plain html of the navbar it would be funny -->\n' );
-// document.write( '<!-- <a class=\"nav-elem\">lyrics</a> -->\n' );
-// document.write( '<a class=\"nav-elem\" href=\"pomes.html\">pomes</a> | <a class=\"nav-elem\" href=\"links.html\">links</a> |\n' );
-// document.write( '<a class=\"nav-elem\" href=\"about.html\">about</a> |\n' );
-// document.write( '<a class=\"nav-elem\" href=\"reccomendations.html\">reccomendations</a> |\n' );
-// document.write( '<a class=\"nav-elem\" href=\"terms.html\">art</a> |\n' );
-// document.write( '<a class=\"nav-elem\" href=\"terms.html\">terms and conditions</a>\n' );
-// document.write( '<!-- <a class=\"nav-elem\">cookies</a> -->\n' );
-// document.write( '<!-- <a class=\"nav-elem\">blog</a> -->\n' );
-// document.write( '</nav>' );
+var variable =
+  "" +
+  '<nav id="navbar">' +
+  '  <a class="nav-elem" href="navbar.html">this the nav bar</a> | <a class="nav-elem" href="index.html">home</a> |' +
+  "  <!-- link to a plain html of the navbar it would be funny -->" +
+  '  <!-- <a class="nav-elem">lyrics</a> -->' +
+  '  <a class="nav-elem" href="pomes.html">pomes</a> | <a class="nav-elem" href="links.html">links</a> |' +
+  '  <a class="nav-elem" href="about.html">about</a> |' +
+  '  <a class="nav-elem" href="reccomendations.html">reccomendations</a> |' +
+  '  <a class="nav-elem" href="terms.html">art</a> |' +
+  '  <a class="nav-elem" href="terms.html">terms and conditions</a>' +
+  '  <!-- <a class="nav-elem">cookies</a> -->' +
+  '  <!-- <a class="nav-elem">blog</a> -->' +
+  "</nav>" +
+  "";
+document.write(variable);
 
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-}
+// function readTextFile(file) {
+//   var rawFile = new XMLHttpRequest();
+//   rawFile.open("GET", file, false);
+//   rawFile.onreadystatechange = function () {
+//     if (rawFile.readyState === 4) {
+//       if (rawFile.status === 200 || rawFile.status == 0) {
+//         var allText = rawFile.responseText;
+//         alert(allText);
+//       }
+//     }
+//   };
+//   rawFile.send(null);
+// }
 
-readTextFile('file://navbar.html');
+// readTextFile("navbar.html");
 
 // fetch("navbar.html")
 //   .then((response) => response.html())
