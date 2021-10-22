2208d7ff8d70e5756257a3f6c2e20e6b7951a25d
rogerShagawat <rogershagawat@gmail.com>
2021-10-22 16:25:54 -0400

    refactoring

--- download_the_internet/javascript/searches.js
+++ download_the_internet/javascript/searches.js
@@ -3,42 +3,29 @@ function searches() {
   this.cost = this.base_cost;
   this.bought = 0;
   this.bits_per_tick = 1;
+
+  calc_delta_bits = function (delta_tick) {
+    let delta_bits = 0;
+    delta_bits = this.bought * this.bits_per_tick * delta_tick;
+    return delta_bits;
+  };
+
   tick = function () {
     let delta_bits = 0;
-    delta_bits = this.bought * this.bits_per_tick;
-
+    delta_bits = calc_delta_bits(1);
     return delta_bits;
   };
 
   buy = function () {
-    let buy_one = function () {
-      console.log(this.cost);
-      if (money < this.cost) {
-        return false;
-      }
-      money = money - this.cost;
-      this.bought++;
-      return true;
-    };
-
-    let new_cost = function () {
-      if (bought > 0) {
-        this.cost = this.base_cost * bought;
-      }
-    };
+    calc_new_cost = function (num_bought) {};
+    buy_helper = function () {};
 
     switch (buy_mode) {
       case "1":
-        buy_one();
         break;
       case "10":
-        let i = 0;
-        while (i < 10 && buy_one()) {
-          i++;
-        }
         break;
       case "max":
-        while (buy_one()) {}
         break;
       default:
         break;
